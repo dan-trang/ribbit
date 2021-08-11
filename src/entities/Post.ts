d7c7de01deb6e47f7ddafa01502a6f3604fdbfe3
Daniel Trang <danieltrangnotes@gmail.com>
2021-08-11 06:11:16 -0700

    resolving merge conflict

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,5 +1,4 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
-<<<<<<< HEAD
 import { Field, Int, ObjectType } from "type-graphql";
 
 @ObjectType()
@@ -18,21 +17,6 @@ export class Post {
   updatedAt = new Date();
 
   @Field()  //comment out field to 'hide' this property
-=======
-
-@Entity()
-export class Post {
-
-  @PrimaryKey()
-  id!: number;
-
-  @Property({ type: "date" })
-  createdAt = new Date();
-
-  @Property({ type: "date", onUpdate: () => new Date() })
-  updatedAt = new Date();
-
->>>>>>> 28bef2bd78a4fd37d2547269b773f6690c3e9398
   @Property({ type: "text" })
   title!: string;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
 import "reflect-metadata";
-=======
->>>>>>> 28bef2bd78a4fd37d2547269b773f6690c3e9398
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
-<<<<<<< HEAD
 import express from 'express'
 import {ApolloServer} from 'apollo-server-express'
 import { buildSchema } from "type-graphql";
@@ -42,17 +38,6 @@ const main = async () => {
     app.listen(4000, () => {
         console.log('server started on localhost:4000');
     });
-=======
-
-const main = async () => {
-    const orm = await MikroORM.init(microConfig);
-    orm.getMigrator().up();
-    // const post = orm.em.create(Post, {title: 'my first post'});
-    // await orm.em.persistAndFlush(post);
-
-    // const posts = await orm.em.find(Post, {});
-    // console.log(posts);
->>>>>>> 28bef2bd78a4fd37d2547269b773f6690c3e9398
 };
 
 main().catch((err) => {
